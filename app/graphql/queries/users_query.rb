7a66a7af1dec0f54e0e590777d81aef302e32844
derek_smits <emrb.2708@gmail.com>
2022-03-15 16:24:23 +0200

    Last fixes

--- app/graphql/queries/users_query.rb
+++ app/graphql/queries/users_query.rb
@@ -1,8 +1,18 @@
 module Queries
   class UsersQuery < Queries::BaseQuery
     type Types::UserType, null: false
-    def resolver
-      "Заглушка"
+    argument :limit,Int, required: false
+
+    def to_h(a)
+      arr = []
+      a.each do |n|
+        user = n.as_json
+        arr.push(user)
+      end
+      arr
+    end
+    def resolve(limit:)
+      to_h(User.all.limit(limit))
     end
   end
 end
