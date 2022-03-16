8dec528cd5d92b8c0c8113ef08145235562d3209
derek_smits <emrb.2708@gmail.com>
2022-03-16 15:24:46 +0200

    ADD limit for all users in GraphQL query

--- app/graphql/queries/users_query.rb
+++ app/graphql/queries/users_query.rb
@@ -1,10 +1,10 @@
 module Queries
   class UsersQuery < Queries::BaseQuery
     type [Types::UserType], null: false
-    # argument :limit,Int, required: false
+    argument :size,Int, required: false
 
-    def resolve()
-      User.all
+    def resolve(size: 10)
+      User.all.limit(size)
     end
   end
 end
